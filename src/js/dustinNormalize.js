82d69cec4576dbbad71a0876fdc6dc72808c18e8
DustinJung <gosoomdoce@gmail.com>
2025-02-26 12:51:25 +0900

    25-02-26 모바일 products marquee 이미지 크기 변경, 추가로 ios 대응 dustinNormalize.js 일부 수정

--- src/js/dustinNormalize.js
+++ src/js/dustinNormalize.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let videos = document.querySelectorAll("video");
 
     videos.forEach(video => {
+        video.setAttribute("playsinline", "");
+        video.setAttribute("webkit-playsinline", "");
+
         video.play().catch(error => {
             console.log("Autoplay blocked. User interaction required.", error);
         });
