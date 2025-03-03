c82da46ec4f795addfdf53c326f26b970997dca7
DustinJung <gosoomdoce@gmail.com>
2025-03-04 05:26:25 +0900

    25-03-04 index.html 설명 페이지 제작, 반응형은 일어나서 학원에서 할 예정정

--- src/forIndex/js/main.js
+++ src/forIndex/js/main.js
@@ -3,14 +3,30 @@ import { initAnimations2 } from "./indexAnimation.js";
 
 function vanila() {
     function theSection() {
-        let h2_list = document.querySelectorAll('.h2_wrapper > h2');
+        let h2_list = document.querySelectorAll('.h2_wrapper li');
+        let curtain = document.querySelector('.theCurtain');
+        let sections = document.querySelectorAll('.section-wrapper2 section')
+
+        let curtainTimeout, sectionTimeout;
 
         h2_list.forEach((h2, i) => {
-            h2.addEventListener('click', () => {
-                this.classList.add('on');
+            h2.addEventListener('click', function() {
+                clearTimeout(curtainTimeout);
+                clearTimeout(sectionTimeout);
                 h2_list.forEach(h => {h.classList.remove('on');});
-            })
-        })
+                this.classList.add('on');
+                curtain.classList.add('on');
+                sectionTimeout = setTimeout(() => {
+                    sections.forEach(sec => {sec.classList.remove('on');});
+                    if(curtain.classList.contains('on')){
+                        sections[i].classList.add('on');
+                    }
+                }, 1000);
+                curtainTimeout = setTimeout(() => {
+                    curtain.classList.remove('on');
+                }, 1500);
+            });
+        });
     }
     theSection();
 }
